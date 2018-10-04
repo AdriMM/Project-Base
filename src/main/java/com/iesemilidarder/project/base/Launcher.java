5ddc190c7ac35e6821d4b015644d90e4ef556627
Adrián Martínez Mateu <32458245+AdriMM@users.noreply.github.com>
2018-10-04 17:56:55 +0200

    asdrftgy

--- src/main/java/com/iesemilidarder/project/base/Launcher.java
+++ src/main/java/com/iesemilidarder/project/base/Launcher.java
@@ -1,4 +1,13 @@
 package com.iesemilidarder.project.base;
 
+import com.iesemilidarder.project.base.data.Moto;
+
 public class Launcher {
+    public static void main (String... args)    {
+        System.out.println("Concesionario brumbrum App 2.0");
+        Moto moto = new Moto();
+        moto.start();
+        System.out.println("pista que voy!");
+        moto.brake();
+    }
 }
