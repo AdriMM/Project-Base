4fe82391a14772e37961a9eab818830c1585af04
Adrián Martínez Mateu <32458245+AdriMM@users.noreply.github.com>
2018-10-22 15:17:15 +0200

    gfhdfh

---
+++ src/main/java/com/iesemilidarder/project/base/BootLaunch.java
@@ -0,0 +1,11 @@
+package com.iesemilidarder.project.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLaunch {
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunch.class, args);
+    }
+}
--- src/main/java/com/iesemilidarder/project/base/MyFirstServiceLauncher.java
+++
@@ -1,4 +0,0 @@
-package com.iesemilidarder.project.base;
-
-public class MyFirstServiceLauncher {
-}
